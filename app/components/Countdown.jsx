cbbc46b7a835eb2f8547da205a5f27d0ac80282b
Marc Ucman <marcucman@yahoo.com>
2017-03-12 23:27:42 -0500

    Add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+
+// var Countdown = () => {
+//   return (
+//     <div>
+//       <h1>Countdown.jsx</h1>
+//       <p>This is where the countdown will go</p>
+//     </div>
+//   )
+// };
+
+var Countdown = React.createClass({
+  render: function () {
+    return (
+      <p>Countdown.jsx</p>
+    )
+  }
+});
+
+module.exports = Countdown;
